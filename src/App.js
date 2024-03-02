7d65922cf052cd273ca072756d545e0092610f35
yusufalp <alpyusuf007@gmail.com>
2024-03-01 21:57:50 -0800

    setup react router and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Easy</h1>
-      </header>
+      <Header />
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }
