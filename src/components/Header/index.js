612b88af38d6985151d038fe856719ebf7a4f7ce
yusufalp <alpyusuf007@gmail.com>
2024-03-12 17:01:24 -0700

    toggle navbar menu depending on page location

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,66 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+import { PATHNAMES } from "../../constants";
 
 function Header() {
+  const location = useLocation();
+
+  const handleNavigation = (route) => {
+    console.log(route);
+  };
+
   return (
     <header className="App-header">
       <nav>
-        <ul>
-          <Link to="/login">Login</Link>
-        </ul>
+        {location.pathname === PATHNAMES.HOME ? (
+          <ul>
+            <li>
+              <button
+                type="button"
+                onClick={() => handleNavigation(PATHNAMES.LOGIN)}
+              >
+                Login
+              </button>
+            </li>
+          </ul>
+        ) : (
+            <ul>
+              <li>
+                <button
+                  type="button"
+                  onClick={() => handleNavigation(PATHNAMES.CREATE)}
+                >
+                  Create Event
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  onClick={() => handleNavigation(PATHNAMES.JOIN)}
+                >
+                  Join Event
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  onClick={() => handleNavigation(PATHNAMES.DASHBOARD)}
+                >
+                  Dashboard
+                </button>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  onClick={() => handleNavigation(PATHNAMES.LOGOUT)}
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          )
+        }
       </nav>
     </header>
   );
