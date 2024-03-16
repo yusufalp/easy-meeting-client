afcc3ebd9dee68fe76631f148763dbc73339355e
yusufalp <alpyusuf007@gmail.com>
2024-03-16 06:03:23 -0700

    build password validation function

--- src/utils/index.js
+++ src/utils/index.js
@@ -14,3 +14,26 @@ export function validateEmail(email) {
 
   return [isValid, message];
 }
+
+export function validatePassword(password, confirmPassword) {
+  // Password must include at least one of each below:
+  // an uppercase, a lowercase, a number and a special character
+  const passwordRegex =
+    /^(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=]).*$/;
+
+  let valid = true;
+  let message = "";
+
+  if (password.length < 10) {
+    valid = false;
+    message = "Password must be at least 10 characters long!";
+  } else if (!passwordRegex.test(password)) {
+    valid = false;
+    message = "Password does not meet criteria!";
+  } else if (password !== confirmPassword) {
+    valid = false;
+    message = "Passwords do no match!";
+  }
+
+  return [valid, message];
+}
