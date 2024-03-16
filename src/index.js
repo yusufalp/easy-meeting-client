d4fa62b3ef31efec97d59d8ac7761e932c0ec090
yusufalp <alpyusuf007@gmail.com>
2024-03-15 23:25:21 -0700

    install redux and setup store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-import "./index.css";
 import App from "./App";
+import store from "./store/store";
+
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
