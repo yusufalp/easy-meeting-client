3374cdda8791e26fdefc44b687196d278ae6fd87
yusufalp <alpyusuf007@gmail.com>
2024-03-21 07:02:38 -0700

    add loading animation

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 import "./Dashboard.css";
 
 function Dashboard() {
-  const [userEvents, setUserEvents] = useState([]);
   const [userEventsDetails, setUserEventsDetails] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const user = useSelector((state) => state.auth.user);
 
@@ -26,8 +26,6 @@ function Dashboard() {
         const result = await response.json();
 
         if (result.success) {
-          setUserEvents(result.data.events);
-
           const eventsDetailsPromises = result.data.events.map(
             async (eventId) => {
               const eventResponse = await fetch(
@@ -58,6 +56,8 @@ function Dashboard() {
         }
       } catch (error) {
         console.error(error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -66,24 +66,27 @@ function Dashboard() {
     }
   }, [user]);
 
-  console.log(userEvents);
-  console.log(userEventsDetails);
-
   return (
-    <div>
-      <h1>Dashboard for {user.firstName}</h1>
-      <h2>Registered Events</h2>
-      <ul>
-        {userEventsDetails &&
-          userEventsDetails.map((userEvent) => (
-            <li key={userEvent._id}>
-              <Link to={`${user._id}/events/${userEvent._id}`}>
-                {userEvent.title}
-              </Link>
-            </li>
-          ))}
-      </ul>
-    </div>
+    <main>
+      <h1>Welcome to your dashboard, {user.firstName}</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : userEventsDetails.length > 0 ? (
+        <>
+          <h2>Registered Events</h2>
+          <ul>
+            {userEventsDetails.map((userEvent) => (
+              <li key={userEvent._id}>
+                <span>{userEvent.title}</span>
+                <Link to={`${user._id}/events/${userEvent._id}`}>Details</Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>There are no registered events</p>
+      )}
+    </main>
   );
 }
 
