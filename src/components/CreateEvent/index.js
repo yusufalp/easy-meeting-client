8e69d215ed5097c22bc109bab26a8a76fb2f6be2
yusufalp <alpyusuf007@gmail.com>
2024-03-27 06:00:41 -0700

    implement create event form and collect form data to state

--- src/components/CreateEvent/index.js
+++ src/components/CreateEvent/index.js
@@ -1,9 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateEvent() {
+  const [createEventFormData, setCreateEventFormData] = useState({
+    title: "",
+    startDate: "",
+    startTime: "09:00",
+    endDate: "",
+    endTime: "17:00",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+
+    setCreateEventFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  console.log(createEventFormData);
+
   return (
     <div>
       <h1>Create Event</h1>
+      <form className="form-bg">
+        <label htmlFor="title">Event Title</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={createEventFormData.title}
+          onChange={handleInputChange}
+          required
+        />
+        <div className="group-row">
+          <div>
+            <label htmlFor="startDate">Start Date</label>
+            <input
+              type="date"
+              id="startDate"
+              name="startDate"
+              max="9999-12-31"
+              value={createEventFormData.startDate}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="startTime">Start Time</label>
+            <input
+              type="time"
+              id="startTime"
+              name="startTime"
+              step="300"
+              value={createEventFormData.startTime}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+        </div>
+        <div className="group-row">
+          <div>
+            <label htmlFor="endDate">End Date</label>
+            <input
+              type="date"
+              id="endDate"
+              name="endDate"
+              max="9999-12-31"
+              min={createEventFormData.startDate}
+              value={createEventFormData.endDate}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="endTime">End Time</label>
+            <input
+              type="time"
+              id="endTime"
+              name="endTime"
+              step="300"
+              min={createEventFormData.startTime}
+              value={createEventFormData.endTime}
+              onChange={handleInputChange}
+              required
+            />
+          </div>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            id="includeWeekends"
+            name="includeWeekends"
+            checked={createEventFormData.includeWeekends}
+            onChange={handleInputChange}
+            required
+          />
+          <label htmlFor="includeWeekends"> Include Weekends</label>
+        </div>
+        <button type="submit">Create</button>
+        <p>* All fields are required</p>
+      </form>
     </div>
   );
 }
